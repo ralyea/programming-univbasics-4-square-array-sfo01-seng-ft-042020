608b592788ec627feb25bebd90b50b9fd7f47755
Paul Ralyea <047par@gmail.com>
2020-03-07 04:51:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,14 @@
 def square_array(array)
-  counter = 0
-  while counter < array.length do
-  puts array.push([counter**=2])
-  counter += 1
+  #counter = 0
+  #while counter < array.length do
+  #@puts array.push([counter**=2])
+  #counter += 1
+  #end
+#end
+
+  squared =[]
+  array.each do |element|
+    squared.push(element **2)
   end
+  squared
 end
